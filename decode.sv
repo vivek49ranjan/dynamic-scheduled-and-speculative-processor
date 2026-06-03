efec2b37d51d7b872821e60681b16f8ddacc0a53
vivek ranjan <vivek49ranjan@gmail.com>
2026-06-03 12:34:20 +0530

    Update_pred_rename.sv

--- decode.sv
+++ decode.sv
@@ -1,14 +1,17 @@
 import config_pkg::*;
 import cpu_types_pkg::*;
+
 module decode ( 
     input  logic                clock,
     input  logic                reset,
+    
     input  logic                if_id_valid,        
+    input  logic                if_id_pred_taken,  
+    input  logic [9:0]          if_id_pred_target, 
     input  logic [9:0]          if_id_pc,
     input  logic [31:0]         if_id_opcode,
     input  logic                stall,              
     input  logic                flush,              
-    input  logic                dispatch_success_i, 
     
     output logic                id_valid,
     output logic [9:0]          id_pc,
@@ -32,10 +35,13 @@ module decode (
         rv_rs2    = if_id_opcode[24:20];
         rv_funct7 = if_id_opcode[31:25];
 
-        comb_dec.operand1_reg = rv_rs1;  
-        comb_dec.operand2_reg = 5'd0; 
-        comb_dec.result_reg   = rv_rd; 
-        comb_dec.pc           = if_id_pc;
+        comb_dec.operand1_reg     = rv_rs1;  
+        comb_dec.operand2_reg     = 5'd0; 
+        comb_dec.result_reg       = rv_rd; 
+        comb_dec.pc               = if_id_pc;
+        
+        comb_dec.predicted_taken  = if_id_pred_taken;  
+        comb_dec.predicted_target = if_id_pred_target; 
 
         case (rv_opcode)
             RV32_OP: begin
@@ -54,11 +60,12 @@ module decode (
                     default: comb_dec.opcode = OPCODE_ADD;
                 endcase
             end
+            
             RV32_OP_IMM: begin
                 comb_dec.instr_type = INSTR_ALU;
                 comb_dec.immediate  = {{20{if_id_opcode[31]}}, if_id_opcode[31:20]}; 
                 case (rv_funct3)
-                    3'b000:  comb_dec.opcode = OPCODE_ADDI; 
+                    3'b000:  comb_dec.opcode = OPCODE_ADD; 
                     3'b001:  comb_dec.opcode = OPCODE_SLL;     
                     3'b010:  comb_dec.opcode = OPCODE_COMPARE; 
                     3'b011:  comb_dec.opcode = OPCODE_COMPARE; 
@@ -66,9 +73,10 @@ module decode (
                     3'b101:  comb_dec.opcode = (rv_funct7[5]) ? OPCODE_SRA : OPCODE_SRL;
                     3'b110:  comb_dec.opcode = OPCODE_OR;      
                     3'b111:  comb_dec.opcode = OPCODE_AND;     
-                    default: comb_dec.opcode = OPCODE_ADDI;
+                    default: comb_dec.opcode = OPCODE_ADD;
                 endcase
             end
+            
             RV32_LOAD: begin
                 comb_dec.instr_type       = INSTR_LOAD;
                 comb_dec.opcode           = OPCODE_LOAD;
@@ -76,6 +84,7 @@ module decode (
                 comb_dec.load_source      = rv_rs1;
                 comb_dec.immediate        = {{20{if_id_opcode[31]}}, if_id_opcode[31:20]};
             end 
+            
             RV32_STORE: begin
                 comb_dec.instr_type        = INSTR_STORE;
                 comb_dec.opcode            = OPCODE_STORE;
@@ -85,6 +94,7 @@ module decode (
                 comb_dec.result_reg        = 5'd0; 
                 comb_dec.immediate         = {{20{if_id_opcode[31]}}, if_id_opcode[31:25], if_id_opcode[11:7]};
             end 
+            
             RV32_BRANCH: begin
                 comb_dec.instr_type = INSTR_BRANCH;
                 comb_dec.operand2_reg = rv_rs2; 
@@ -101,22 +111,32 @@ module decode (
                 endcase
             end
 
+            
             RV32_LUI, RV32_AUIPC: begin
-                comb_dec.instr_type = INSTR_ALU; 
-                comb_dec.opcode     = OPCODE_ADD; 
-                comb_dec.immediate  = {if_id_opcode[31:12], 12'b0}; 
+                comb_dec.instr_type   = INSTR_ALU; 
+                
+                comb_dec.opcode       = OPCODE_ADD; 
+                
+                comb_dec.operand1_reg = 5'd0; 
+                comb_dec.operand2_reg = 5'd0; 
+                
+                comb_dec.result_reg   = rv_rd;
+                comb_dec.immediate    = {if_id_opcode[31:12], 12'b0}; 
             end
 
             RV32_JAL: begin
-                comb_dec.instr_type = INSTR_BRANCH; 
-                comb_dec.opcode     = JE; 
-                comb_dec.immediate  = {{12{if_id_opcode[31]}}, if_id_opcode[19:12], if_id_opcode[20], if_id_opcode[30:21], 1'b0};
+                comb_dec.instr_type   = INSTR_BRANCH; 
+                comb_dec.opcode       = JE; 
+                comb_dec.operand1_reg = 5'd0; 
+                comb_dec.operand2_reg = 5'd0; 
+                comb_dec.immediate    = {{12{if_id_opcode[31]}}, if_id_opcode[19:12], if_id_opcode[20], if_id_opcode[30:21], 1'b0};
             end
 
             RV32_JALR: begin
-                comb_dec.instr_type = INSTR_BRANCH; 
-                comb_dec.opcode     = JE;
-                comb_dec.immediate  = {{20{if_id_opcode[31]}}, if_id_opcode[31:20]};
+                comb_dec.instr_type   = INSTR_BRANCH; 
+                comb_dec.opcode       = JE;
+                comb_dec.operand2_reg = rv_rs1; 
+                comb_dec.immediate    = {{20{if_id_opcode[31]}}, if_id_opcode[31:20]};
             end
 
             default: begin
@@ -126,6 +146,7 @@ module decode (
             end
         endcase
     end
+
     always_ff @(posedge clock or posedge reset) begin
         if (reset) begin
             id_pc               <= 10'd0;
