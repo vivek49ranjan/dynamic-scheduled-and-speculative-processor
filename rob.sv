dc8c342a8445d111a769915599a4b75ac216fb93
vivek ranjan <vivek49ranjan@gmail.com>
2026-06-03 12:39:13 +0530

    Updated_pred_rename.sv

--- rob.sv
+++ rob.sv
@@ -4,38 +4,63 @@ import config_pkg::*;
 module rob (
     input  logic        clk, reset,
     input  logic        pipeline_flush_i, 
-    input  logic        rob_allocate_valid_i,
-    input  rob_instruction_metadata_t rob_allocate_data_i,
+    
+    input  logic        rob_alloc_valid_i,
+    output logic        rob_full_o,
+    output logic [4:0]  rob_alloc_idx_o,
+    
+    input  logic        rob_fill_valid_i,
+    input  logic [4:0]  rob_fill_idx_i,
+    input  rob_instruction_metadata_t rob_fill_data_i,
+    
     input  logic        wb_valid_i,
     input  logic [4:0]  wb_rob_tag_i,
     input  logic [31:0] wb_result_val_i, 
-    input  logic        commit_ready_i, 
     input  logic        wb_branch_mispredict_i, 
-
-    output logic        rob_full_o,
-    output logic [4:0]  rob_next_idx_o,
+    
+    input  logic        commit_ready_i, 
     output logic        commit_valid_o,
     output rob_entry_t  commit_data_o,
+	 
     output logic [4:0]  head_ptr_o,
+	 
     output logic        flush_o,
     output logic [31:0] flush_pc_o,
-    output logic [9:0]  flush_branch_pc_o 
+    output logic [9:0]  flush_branch_pc_o,
+
+    input  logic [4:0]  read_idx1_i,
+    output logic        read_ready1_o,
+    output logic        read_busy1_o,
+    output logic [31:0] read_val1_o,
+    
+    input  logic [4:0]  read_idx2_i,
+    output logic        read_ready2_o,
+    output logic        read_busy2_o,
+    output logic [31:0] read_val2_o
 );
 
     rob_entry_t rob_file [0:31]; 
     logic [4:0] head_ptr, tail_ptr;
     logic [5:0] count; 
 
-    assign head_ptr_o     = head_ptr;
-    assign rob_next_idx_o = tail_ptr;
-    assign rob_full_o     = (count >= 6'd32);
+    assign head_ptr_o      = head_ptr;
+    assign rob_alloc_idx_o = tail_ptr; 
+    assign rob_full_o      = (count >= 6'd32);
 
-    assign commit_valid_o = (count != 0) && rob_file[head_ptr].busy && rob_file[head_ptr].is_complete;
-    assign commit_data_o  = rob_file[head_ptr];
+    assign commit_valid_o  = (count != 0) && rob_file[head_ptr].busy && rob_file[head_ptr].is_complete;
+    assign commit_data_o   = rob_file[head_ptr];
     
     assign flush_o           = commit_valid_o && commit_ready_i && commit_data_o.inst_data.is_mispredicted;
     assign flush_pc_o        = commit_data_o.result_value; 
     assign flush_branch_pc_o = commit_data_o.inst_data.pc[9:0];
+
+    assign read_ready1_o = rob_file[read_idx1_i].is_complete;
+    assign read_busy1_o  = rob_file[read_idx1_i].busy;
+    assign read_val1_o   = rob_file[read_idx1_i].result_value;
+
+    assign read_ready2_o = rob_file[read_idx2_i].is_complete;
+    assign read_busy2_o  = rob_file[read_idx2_i].busy;
+    assign read_val2_o   = rob_file[read_idx2_i].result_value;
 
     always_ff @(posedge clk or posedge reset) begin
         if (reset) begin
@@ -61,11 +86,14 @@ module rob (
                 rob_file[wb_rob_tag_i].inst_data.is_mispredicted <= wb_branch_mispredict_i;
             end
 
-            case ({ (rob_allocate_valid_i && !rob_full_o), (commit_valid_o && commit_ready_i) })
+            if (rob_fill_valid_i) begin
+                rob_file[rob_fill_idx_i].inst_data <= rob_fill_data_i;
+            end
+
+            case ({ (rob_alloc_valid_i && !rob_full_o), (commit_valid_o && commit_ready_i) })
                 2'b10: begin 
                     rob_file[tail_ptr].busy        <= 1'b1;
                     rob_file[tail_ptr].is_complete <= 1'b0;
-                    rob_file[tail_ptr].inst_data   <= rob_allocate_data_i;
                     tail_ptr <= tail_ptr + 5'd1;
                     count    <= count + 6'd1;
                 end
@@ -77,7 +105,6 @@ module rob (
                 2'b11: begin 
                     rob_file[tail_ptr].busy        <= 1'b1;
                     rob_file[tail_ptr].is_complete <= 1'b0;
-                    rob_file[tail_ptr].inst_data   <= rob_allocate_data_i;
                     tail_ptr <= tail_ptr + 5'd1;
                     
                     rob_file[head_ptr].busy        <= 1'b0;
