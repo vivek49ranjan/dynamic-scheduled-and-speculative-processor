d76de597b7a7489dbd85fa2bad4e5545245de592
vivek ranjan <vivek49ranjan@gmail.com>
2026-06-03 12:33:18 +0530

    Updated_rename_branch.sv

--- commit.sv
+++ commit.sv
@@ -1,23 +1,26 @@
 import cpu_types_pkg::*;
+
 module commit_stage (
     input  logic               clock, reset,
     input  rob_entry_t         rob_head_entry_i,
-    input  logic               rob_empty_i,     
+    input  logic               commit_valid_i,     
     input  logic               lsq_ready_to_commit_i,
     
     output logic               reg_write_en_o,
     output logic [4:0]         reg_write_addr_o,
     output logic [31:0]        reg_write_data_o,
-    output logic               commit_en_o    
+    output logic               commit_en_o,
+    output logic               commit_store_req_o  
 );
 
     always_comb begin
-        reg_write_en_o   = 1'b0;
-        reg_write_addr_o = 5'd0;
-        reg_write_data_o = 32'd0;
-        commit_en_o      = 1'b0;
+        reg_write_en_o     = 1'b0;
+        reg_write_addr_o   = 5'd0;
+        reg_write_data_o   = 32'd0;
+        commit_en_o        = 1'b0;
+        commit_store_req_o = 1'b0;
 
-        if (!rob_empty_i && rob_head_entry_i.busy && rob_head_entry_i.is_complete) begin
+        if (commit_valid_i && rob_head_entry_i.busy && rob_head_entry_i.is_complete) begin
             
             if (rob_head_entry_i.inst_data.instr_type == INSTR_ALU || 
                 rob_head_entry_i.inst_data.instr_type == INSTR_LOAD) begin
@@ -30,7 +33,10 @@ module commit_stage (
             end
 
             if (rob_head_entry_i.inst_data.instr_type == INSTR_STORE) begin
-                if (lsq_ready_to_commit_i) commit_en_o = 1'b1;
+                commit_store_req_o = 1'b1; 
+                if (lsq_ready_to_commit_i) begin
+                    commit_en_o = 1'b1;   
+                end
             end else begin
                 commit_en_o = 1'b1;
             end
